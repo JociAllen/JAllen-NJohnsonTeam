65a26a09c4f3b72009672ec5ad4104d075b586e7
AllenMac <AllenMac@192.168.0.3>
2016-01-29 22:57:21 -0700

    1/29/2016-initial commit to repository

---
+++ LehisJourney/src/lehisjourney/LehisJourney.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lehisjourney;
+
+import byui.cit230.lehisJourney.model.Player;
+
+/**
+ *
+ * @author AllenMac
+ */
+public class LehisJourney {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Player playerOne = new Player();
+        
+        playerOne.setName("Nephi");
+        playerOne.setBestTime (7.00);
+        
+        String playerInfo = playerOne.toString();
+        System.out.println(playerInfo);
+        
+        
+    }
+    
+}
