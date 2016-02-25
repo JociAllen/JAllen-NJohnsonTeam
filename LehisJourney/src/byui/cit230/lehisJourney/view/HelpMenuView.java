16b740203bcce225af3ebbad4c71b9a824d29fc0
AllenMac <AllenMac@192.168.0.3>
2016-02-24 21:09:17 -0700

    2/24/2016- Help Menu Adjust

---
+++ LehisJourney/src/byui/cit230/lehisJourney/view/HelpMenuView.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit230.lehisJourney.view;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author AllenMac
+ */
+public class HelpMenuView {
+    
+    private final String menu = "\n"
+            + "\n H - Help Topics"
+            + "\n T - Tutorial"
+            + "\n M - Main Menu"
+            + "\n Q - Quit";
+
+
+    public void displayMenu() {
+
+        char selection = ' ';
+
+        do {
+            System.out.println(menu);
+
+            String input = getInput();
+            selection = input.charAt(0);
+
+        } while (selection != 'Q');
+
+    }
+
+    public String getInput() {
+        Scanner keyboard = new Scanner(System.in);
+        String input = null;
+        boolean isValid = false;
+
+        while (!isValid) {
+            //System.out.println("Invalid input. Please enter a valid character.");
+            input = keyboard.nextLine();
+            input = input.trim();
+
+            if (input == null || input.length() == 0) {
+
+            } else {
+                isValid = true;
+            }
+        }
+
+        return input;
+    }
+}
+
+
